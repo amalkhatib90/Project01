b7787e3efe0b398eb75c4ff68644b9a469384228
Ayah Alkhatib <ayah.alkhatib90@gmail.com>
2018-09-13 21:24:38 +0100

    This version has a loop in menu to play aqain after the game ends

--- menu.py
+++ menu.py
@@ -6,8 +6,9 @@
 #  Created: 09/08/18
 #  Completed:
 
+from executive import Executive
 
-class Menu:
+class Menu :
 
     def __init__(self):
         self.choice = 2
@@ -15,30 +16,51 @@ class Menu:
         self.board_size = 0
         #self.game_over = game_over
         self.is_loser = False
+        self.myGame = Executive ()
+    
 
-
-#    def game_menu(self):
-#       print("Welcome to Minesweeper!")
-#       print("Please, chose from the menu:")
-        print("""1. Play the Game
+    def game_menu(self):
+        while (self.myGame.game_over == False):
+            
+            print("Please, chose from the menu:")
+            print("""1. Play the Game
 2. Quit""")
-#        self.choice = int(input())
-#        if self.choice == 1:
-#            print("Please Enter the board size, it should be at least 2, and maximum 15")
-#           self.board_size = int(input())
-#           print("Awesome! Let the fun begin!")
-#           max_mines = self.board_size ** 2 - 1
-#           print("Enter the number of mines, it should be between 1 and " + str(max_mines))
-#           self.mines_num = int(input())
+            self.choice = int(input())
+            if self.choice == 1:
+                self.myGame.setup()
+                self.myGame.play()
+            else:
+                print("Goodbye! See you later!")
+                return
+            play_again = input ("Play again (p), otherwise (q): ")
+            if play_again == "p":
+                self.myGame.setup()
+                self.myGame.play()
+            else:
+                print ("Goodbye! See you later!")
+                return
 
-#       else:
-#           return
+    def game_rules (self):
+        print ("""Welcome to Minesweepers!
+
+Here are the game instructions:
+    * The game will provid players a square board with a number of hidden mines and similar number of flags.
+    * Player has three choices of action:
+        - Flag: to flag squares that might have mines.
+        - Unflag: to unflag if player changed mind.
+        - Reveal: to see what squares have underneath.
+    * When player chose to reveal:
+        - If the square has a mine -> Gameover!
+        - If not a mine, it will show spaces and numbers to tell player the number of mines around the chosen square.
+    * The goal is to flag all mines until the counter of flags equals to zero without revealing any mine.
+Good Luck!
+""")
 
     def play_again(self):
         if self.is_winner:
             print("YOU WON!!")
             self.game_menu()
-           self.is_winner = False
+            self.is_winner = False
         elif self.is_loser:
             print("GAME OVER!")
             self.game_menu()
